5324c91333ca7c1a1149a7120e38f349a3ce56e4
Lining Guan <lining.guan@grabtaxi.com>
2020-03-23 21:01:35 +0800

    install php and extension in mac

---
+++ mac/php.sh
@@ -0,0 +1,16 @@
+brew search php
+brew install php@7.3
+brew install php@7.2
+brew install brew-php-switcher
+brew-php-switcher php@7.2
+
+# php extension
+pecl version
+pecl help
+pecl search xdebug
+pecl install xdebug
+php install http://pecl.php.net/get/redis-4.2.0.tgz
+
+php -m
+php -c
+php --ini
